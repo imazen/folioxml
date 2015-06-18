706b8eda60805e837cc827d95dbefebd2c4c9c32
Nathanael Jones <nathanael.jones@gmail.com>
2015-06-18 18:19:47 -0400

    Add NodeTextTokenWrapper (removed with TextTokenList modifications)

---
+++ core/folioxml/src/folioxml/text/NodeTextTokenWrapper.java
@@ -0,0 +1,28 @@
+package folioxml.text;
+
+import folioxml.core.TokenBase;
+import folioxml.xml.Node;
+
+/**
+ * Created by nathanael on 6/18/15.
+ */
+public class NodeTextTokenWrapper  implements ITextToken {
+    private Node n;
+
+    public NodeTextTokenWrapper(Node n) {
+        this.n = n;
+    }
+
+    public String getText() {
+        return n.markup;
+    }
+
+    public void setText(String s) {
+        n.markup = s;
+        n.type = TokenBase.TokenType.Text;
+        if (s.length() == 0) {
+            //Delete
+            n.remove(true); //Delete token from parent.
+        }
+    }
+}
