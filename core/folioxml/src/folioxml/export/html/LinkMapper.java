e1e70b60a16c3b7c4796b3ef56a2b4deb12691f1
Nathanael Jones <nathanael.jones@gmail.com>
2015-09-01 16:51:21 -0400

    Add uri->uri link remapping

--- core/folioxml/src/folioxml/export/html/LinkMapper.java
+++ core/folioxml/src/folioxml/export/html/LinkMapper.java
@@ -25,6 +25,9 @@ public class LinkMapper implements NodeListProcessor, ExportingNodeListProcessor
 
     private HashMap<String, Object> infobaseToUriMap;
 
+
+    private HashMap<String, Object> uriToUriMap;
+
     public LinkMapper(InfobaseConfigBase c){
         this.c = c;
 
@@ -33,6 +36,9 @@ public class LinkMapper implements NodeListProcessor, ExportingNodeListProcessor
         if (mapperConfig.get("infobases") != null) {
             infobaseToUriMap = (HashMap<String,Object>) mapperConfig.get("infobases");
         }
+        if (mapperConfig.get("urls") != null) {
+            uriToUriMap = (HashMap<String,Object>) mapperConfig.get("urls");
+        }
     }
 
     @Override
@@ -40,15 +46,23 @@ public class LinkMapper implements NodeListProcessor, ExportingNodeListProcessor
 
         NodeList links = nodes.filterByTagName("a|link", true);
         for (Node n:links.list()) {
+            String newUrl = null;
+
             String infobase = n.get("infobase");
-            if (infobase == null) continue;
-            if (infobaseToUriMap.containsKey(infobase)) {
+            if (infobase != null && infobaseToUriMap != null && infobaseToUriMap.containsKey(infobase)) {
+                newUrl =  (String) infobaseToUriMap.get(infobase);
+            }
+            String href = n.get("href");
+            if (href != null && uriToUriMap != null && uriToUriMap.containsKey(href)) {
+                newUrl =  (String) uriToUriMap.get(href);
+            }
+
+            if (newUrl != null){
                 String old = n.toXmlString(true);
                 n.setTagName("a");
-                n.set("href", (String) infobaseToUriMap.get(infobase));
                 n.removeAttr("infobase");
+                n.set("href", newUrl);
                 provider.getNamedStream("mapped_links").append(" in record ").append(n.rootNode().get("folioId")).append("\n").append(old).append(" ... to:\n").append(n.toXmlString((true))).append("\n");
-
             }
 
         }
